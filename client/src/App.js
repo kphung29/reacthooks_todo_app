28e8de572527a8d3c91364b0dfce3e861f7a37ee
kphung29 <kphung29@gmail.com>
2019-11-10 11:00:33 -0500

    styling skeleton for App component using material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Typography from '@material-ui/core/Typography';
 
 const App = () => {
-  return <div className="app">hello world!</div>;
+  return <div className="app">
+    <Typography component='h1' variant='h2'>
+      Todos
+    </Typography>
+    </div>;
 };
 
 export default App;
