0aee745b7983949400ce6a3c0d0d8c226ed9bb2b
kphung29 <kphung29@gmail.com>
2019-11-10 13:33:52 -0500

    created TodoList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
 /* eslint-disable react/jsx-filename-extension */
-import React from "react";
+import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
 import TodoForm from "./components/TodoForm";
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="app">
       <Typography component="h1" variant="h2">
